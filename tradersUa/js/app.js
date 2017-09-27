f0ebb20f55c12e1e9b11f0cc1912db31e99da965
barbiture <delightrif@gmail.com>
2017-09-27 14:28:03 +0300

    fb.log() v1.15

--- tradersUa/js/app.js
+++ tradersUa/js/app.js
@@ -40,19 +40,25 @@ window.fbAsyncInit = function() {
       FB.login();
     }
   });
-    
-    
   };
-  
+  // (function(d, s, id) {
+  //   var js, fjs = d.getElementsByTagName(s)[0];
+  //   if (d.getElementById(id)) return;
+  //   js = d.createElement(s); js.id = id;
+  //   js.src = "https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.10&appId=1920488714856591";
+  //   fjs.parentNode.insertBefore(js, fjs);
+  // }(document, 'script', 'facebook-jssdk'));
+
   (function(d, s, id){
      var js, fjs = d.getElementsByTagName(s)[0];
      if (d.getElementById(id)) {return;}
      js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
+     // js.src = "//connect.facebook.net/en_US/sdk.js";
      // js.src = "https://connect.facebook.net/en_US/sdk.js";
+     js.src = "https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.10&appId=1920488714856591";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
-    
+
 
 
 //# sourceMappingURL=maps/app.js.map
